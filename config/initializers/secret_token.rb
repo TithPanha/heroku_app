9d5978c0885aaa7881857f056caeeeb57f28c21c
KL <kl@kl.(none)>
2013-04-05 10:09:41 +0700

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HerokuApp::Application.config.secret_token = 'eb756700d6bf1c849223d28672418a23114205e07a0c3fb4478c998221a534b6fca9bd062b05a949b9759d3ba6652bd48c2b6ec3dd72d2e8783feca55c9e20ca'
